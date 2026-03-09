44e90a627f2274e267b9715dc75695c69802d965
simsa <simsaaav@mail.ru>
2026-03-09 19:17:46 +0300

    accordion

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ sliders.forEach((slider, index) => {
   });
 
 });
+
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+
+  header.addEventListener('click', () => {
+    const isOpen = item.classList.contains('accordion-item--active');
+
+    // Закрываем все
+    accordionItems.forEach(el => {
+      el.classList.remove('accordion-item--active');
+    });
+
+    // Открываем текущий, если он был закрыт
+    if (!isOpen) {
+      item.classList.add('accordion-item--active');
+    }
+  });
+});
