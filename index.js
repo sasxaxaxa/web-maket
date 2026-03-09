75c7068a13a2604efdcd043c3827520761529b5b
simsa <simsaaav@mail.ru>
2026-03-09 03:15:43 +0300

    styles & slider

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Happy developing ✨')
+const sliders = document.querySelectorAll('.route__slider');
+
+sliders.forEach((slider, index) => {
+
+  new Swiper(slider, {
+
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    centeredSlides: true,
+
+    initialSlide: index === 1 ? 2 : 0, // второй блок открывается с 3 картинки
+
+    pagination: {
+      el: slider.querySelector('.swiper-pagination'),
+      clickable: true
+    }
+
+  });
+
+});
